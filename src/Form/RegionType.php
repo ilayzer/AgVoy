5e9ba51f0b41297e58282039d00e76d3320b4033
clement.gloux@telecom-sudparis.eu <c. gloux>
2019-10-31 00:00:10 +0100

    Plusieurs trucs, notamment les crud Region
    Signed-off-by: clement.gloux@telecom-sudparis.eu <C. Gloux>

---
+++ src/Form/RegionType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Region;
+use App\Entity\Room;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('presentation', TextareaType::class)
+            ->add('country', TextType::class)
+            ->add('rooms', EntityType::class, [
+                'class' => Room::class,
+                'choice_label' => 'summary',
+                'multiple' => true,
+                'expanded' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Region::class,
+        ]);
+    }
+}
